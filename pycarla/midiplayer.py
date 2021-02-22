4c4b86cf4cf0418c7c8dc1399e227faea597a78a
Federico Simonetta <federico.simonetta@unimi.it>
2021-02-22 17:52:08 +0100

    Finally MIDIPlayer uses callback and supports freewheel!

--- pycarla/midiplayer.py
+++ pycarla/midiplayer.py
@@ -1,46 +1,142 @@
+import fnmatch
 import multiprocessing
 import os
 import shutil
+import threading
 import time
-from typing import Union
+from typing import Any, List, Union
 
+import jack
 import mido
 
-from .utils import ExternalProcess, get_smf_duration, progressbar, Popen
+from .utils import ExternalProcess, Popen, get_smf_duration, progressbar
 
 
 class MIDIPlayer():
 
-    MIDI_PORT = 'carla'
+    MIDI_PORT = 'Carla:events-in*'
 
-    def __init__(self):
+    def __init__(self, freewheel=False):
         """
         Creates a player which is able to connect to a Carla instance
+
+        Optionally, `freewheel` cab be used to start freeewheel mode before of
+        playing.
 
         For now, only one Carla instance should be active.
         """
         super().__init__()
-        self._update_port()
+        self.connected = False
+        self.freewheel = freewheel
+
+    def _setup(self):
+        self.client = jack.Client('pycarla-midiplayer')
+        self.port = self.client.midi_outports.register('output')
+        if self._is_freewheel != self.freewheel:
+            self.client.set_freewheel(self.freewheel)
+            self._is_freewheel = self.freewheel
 
     def _update_port(self):
-        for port in mido.get_output_names():
-            if self.MIDI_PORT in port.lower():
-                self.port = port
-                break
+        real_ports = [port.name for port in self.client.get_ports()]
+        for port in real_ports:
+            if fnmatch.fnmatch(port, self.MIDI_PORT):
+                if not self.connected or not self.port.is_connected_to(port):
+                    try:
+                        self.port.connect(port)
+                    except Exception:
+                        print("Cannot connect to Carla. Is the client active?")
+                        self.connected = False
+                    else:
+                        self.connected = True
 
-    def send_message(self, message: mido.Message, reset: bool = False):
+    def wait(self):
+        if hasattr(self, 'process'):
+            self.process.wait()
+
+    def synthesize_messages(self,
+                            messages: List[mido.Message],
+                            sync=True,
+                            progress=True,
+                            max_dur=1e15) -> multiprocessing.Process:
         """
-        0. If not yet done, look a for a Carla instance
-        1. Open a Jack port
-        2. Send a message to the Carla instance
-        3. Reset control changes is `reset` is True
-        4. Close the port
+        Synthesize a list of messages
+
+        0. Create a jack client if not yet done
+        1. Connect the port of this jack client to Carla if not yet done
+        2. Sort messages by time
+        3. Send the list of messages to the Carla instance
+
+        The process happens in a separate process saved in `self.process`
+
+        If `sync` is True, this function waits until all messages have been
+        processed, otherwise, it suddenly returns. You can wait by calling the
+        `wait` method of this object.
+
+        If `progress` is True, a bar is printed showing the progress of the
+        synthesis. If `sync` is False, `progress` is not used.
+
+        `max_dur` is the maximum time for after which messages are discarded
+
+        This function is compatible with freewheeling mode.  Freewheel prevents
+        jack from waiting between return calls. This allows for the maximum
+        allowed speed, but not output/input operation is done with system audio
+        (i.e. you cannot listen/recording to anything while in freewheeling
+        mode)
+
+        Note: Mido numbers channels 0 to 15 instead of 1 to 16. This makes them
+        easier to work with in Python but you may want to add and subtract 1
+        when communicating with the user.
         """
-        if not hasattr(self, 'port'):
+        messages.sort(key=lambda x: x.time)
+        self._messages = messages
+        dur = min(max_dur, self._messages[-1].time)
+        self.process = multiprocessing.Process(target=self._play)
+        self.process.start()
+
+        if sync:
+            _wait_dur(dur, progress, condition=self.process.is_alive)
+            self.process.terminate
+
+        return self.process
+
+    def _play(self):
+        event = threading.Event()
+        self._setup()
+
+        global msg, offset
+        it = iter(self._messages)
+        msg = next(it)
+        offset = msg.time
+
+        @self.client.set_process_callback
+        def process(frames):
+            global offset, msg
+            self.port.clear_buffer()
+            while True:
+
+                if offset >= frames:
+                    # wait for the correct block
+                    offset -= frames
+                    return
+                # Note: This may raise an exception:
+                self.port.write_midi_event(offset, msg.bytes())
+
+                try:
+                    msg = next(it)
+                except StopIteration:
+                    event.set()
+                    raise jack.CallbackExit
+                offset += round(msg.time * fs)
+
+        @self.client.set_samplerate_callback
+        def samplerate(sr):
+            global fs
+            fs = sr
+
+        with self.client:
+            # cannot connect if it's not active and running the callback...
             self._update_port()
-
-        with mido.open_output(self.port, autoreset=reset) as outport:
-            outport.send(message)
+            event.wait()
 
     def synthesize_midi_note(self,
                              pitch: int,
@@ -51,119 +147,69 @@ class MIDIPlayer():
                              sostenuto: int = 0,
                              channel: int = 0,
                              program: int = 0,
-                             reset: bool = False):
+                             **kwargs) -> multiprocessing.Process:
         """
-        0. If not yet done, look a for a Carla instance
-        1. Open a Jack port
-        2. Send note_on, note_off, and control changes according to parameters
-           to the Carla instance
-        3. Reset control changes is `reset` is True
-        4. Close the port
-
-        Note: Mido numbers channels 0 to 15 instead of 1 to 16. This makes them
-        easier to work with in Python but you may want to add and subtract 1
-        when communicating with the user.
+        set up a list of messages representing one note and then calls
+        `self.synthesize_messages`. All keywords from that method can be used
+        here.
         """
-        if not hasattr(self, 'port'):
-            self._update_port()
-
-        with mido.open_output(self.port, autoreset=reset) as outport:
-            # program
-            outport.send(
-                mido.Message('program_change',
-                             program=program,
-                             channel=channel))
+        messages = [
+            # program change
+            mido.Message('program_change',
+                         program=program,
+                         channel=channel,
+                         time=0),
             # sustain
-            outport.send(
-                mido.Message('control_change',
-                             control=64,
-                             value=sustain,
-                             channel=channel))
+            mido.Message('control_change',
+                         control=64,
+                         value=sustain,
+                         channel=channel,
+                         time=0),
             # sostenuto
-            outport.send(
-                mido.Message('control_change',
-                             control=66,
-                             value=sostenuto,
-                             channel=channel))
+            mido.Message('control_change',
+                         control=66,
+                         value=sostenuto,
+                         channel=channel,
+                         time=0),
             # soft
-            outport.send(
-                mido.Message('control_change',
-                             control=67,
-                             value=soft,
-                             channel=channel))
+            mido.Message('control_change',
+                         control=67,
+                         value=soft,
+                         channel=channel,
+                         time=0),
             # note on
-            outport.send(
-                mido.Message('note_on',
-                             velocity=velocity,
-                             note=pitch,
-                             channel=channel))
-            time.sleep(duration)
+            mido.Message('note_on',
+                         velocity=velocity,
+                         note=pitch,
+                         channel=channel,
+                         time=0),
             # note off
-            outport.send(mido.Message('note_off', note=pitch, channel=channel))
+            mido.Message('note_off',
+                         note=pitch,
+                         channel=channel,
+                         time=duration)
+        ]
 
-    def synthesize_midi_file(self,
-                             midifile: Union[mido.MidiFile, str],
-                             sync=True,
-                             progress=True,
-                             max_dur=1e15) -> multiprocessing.Process:
+        return self.synthesize_messages(messages, **kwargs)
+
+    def synthesize_midi_file(self, midifile: Any,
+                             **kwargs) -> multiprocessing.Process:
         """
-        Send midi messages contained in `filename` to `self.port`
+        Send midi messages contained in `filename` using
+        `self.synthesize_messages`. All keywords from that method can be used
+        here.
 
         `midifile` can be a `mido.MidiFile` object or a string
 
-        `self.port` is initialized during in the constructor of this object but
-        if no Carla instance is found it is automatically updated when
-        enetering this method. You can force an update with `self._update_port`
-        method.
-
-        If `sync` is set to False, a new sub-process is dispached and this
-        function returns suddenly.  The `multiprocessing.Process` object is
-        returned and you can wait for that process to terminate (e.g. by
-        calling its `join` method). You can also wait by calling the `wait`
-        method of this object.
-
-        If `sync` is True, a new process is dispatched, but it is waited in
-        before returning.
-
-        If `progress` is True, a bar is printed showing the progress of the
-        synthesis. If `sync` is False, `progress` is not used.
-
-        `max_dur` is the maximum time for which is allowed to play
-
         After the playback, ports are resetted
         """
-        if not hasattr(self, 'port'):
-            self._update_port()
 
         if type(midifile) is str:
             midifile = mido.MidiFile(midifile)
 
-        dur = min(midifile.length, max_dur)  # type: ignore
+        messages = [m for track in midifile.tracks for m in track]
 
-        def play():
-            with mido.open_output(self.port, autoreset=True) as outport:
-                for i, msg in enumerate(midifile.play()):
-                    if msg.time >= dur:
-                        break
-                    outport.send(msg)
-                outport.reset()
-
-        self.process = multiprocessing.Process(target=play)
-        self.process.start()
-
-        if sync:
-            _wait_dur(dur, progress)
-            self.process.terminate()
-
-        return self.process
-
-    def wait(self):
-        """
-        Wait for this object has finished sending midi events
-        """
-        if not hasattr(self, 'process'):
-            return
-        self.process.join()
+        return self.synthesize_messages(messages, **kwargs)
 
 
 class ExternalMIDIPlayer(ExternalProcess):
@@ -240,6 +286,7 @@ PATH")
                          note=pitch,
                          time=duration,
                          channel=channel))
+        mid.tracks.append(track)
         mid.save(self._tempname)
         self._duration = duration
         self._start_player(self._tempname)
@@ -281,7 +328,9 @@ PATH")
         self._start_player(midifile)
 
         if sync:
-            _wait_dur(self._duration, progress)
+            _wait_dur(self._duration,
+                      progress,
+                      condition=lambda: self.process.poll() is None)
             self.kill()
         return self.process
 
@@ -293,10 +342,10 @@ PATH")
             os.remove(self._tempname)
 
 
-def _wait_dur(dur, progress):
+def _wait_dur(dur, progress, condition=lambda: True):
     q = 0.005
     i = 0.0
-    while i < dur:
+    while i < dur and condition():
         if progress:
             progressbar(i, 1, dur, status='Synthesis')
         time.sleep(q)

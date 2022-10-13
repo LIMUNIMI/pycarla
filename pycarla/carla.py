15ba6003c2b9915c32ffe00b7e09a77044fb35d8
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-13 06:09:03 +0000

    Adding tarfile member sanitization to extractall()

--- pycarla/carla.py
+++ pycarla/carla.py
@@ -45,7 +45,26 @@ def download():
         rand = str(random.randint(10**4, 10**5))
         tmp_path = "/tmp/carla" + "-" + rand
         with tarfile.open(fname, "r:xz") as file:
-            file.extractall(tmp_path)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(file, tmp_path)
 
         shutil.move(tmp_path + "/Carla_" + version + "-linux" + arch,
                     CARLA_PATH)
